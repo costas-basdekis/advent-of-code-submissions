d6944e0fb146ea161dbb8857be14098f9bc33658
Costas Basdekis <costas.basdekis@gmail.com>
2020-11-20 11:14:03 +0000

    Submit 2020-25-A

---
+++ year_2020/day_25/part_a.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+import itertools
+
+import utils
+
+
+class Challenge(utils.BaseChallenge):
+    def solve(self, _input):
+        """
+        >>> Challenge().default_solve()
+        17032383
+        """
+        public_key_a_str, public_key_b_str = _input.strip().splitlines()
+        public_key_a = int(public_key_a_str)
+        public_key_b = int(public_key_b_str)
+        return get_encryption_key(public_key_a, public_key_b)
+
+
+def get_encryption_key(public_key_a, public_key_b):
+    """
+    >>> get_encryption_key(17807724, 5764801)
+    14897079
+    """
+    if public_key_b < public_key_a:
+        public_key_a, public_key_b = public_key_b, public_key_a
+
+    loop_size_a = find_loop_size(7, public_key_a)
+    return transform_subject_number(public_key_b, loop_size_a)
+
+
+def transform_subject_number(subject_number, loop_size):
+    """
+    >>> transform_subject_number(7, 8)
+    5764801
+    """
+    for value_loop_size, value in iterate_subject_number(subject_number):
+        if value_loop_size == loop_size:
+            return value
+
+    raise Exception(f"Finished endless loop")
+
+
+def iterate_subject_number(subject_number):
+    value = 1
+    for loop_size in itertools.count():
+        yield loop_size, value
+        value *= subject_number
+        value = value % 20201227
+
+
+def unlock(card_loop_size, door_loop_size):
+    card_public_key = transform_subject_number(7, card_loop_size)
+    door_public_key = transform_subject_number(7, door_loop_size)
+    encryption_key = transform_subject_number(door_public_key, card_loop_size)
+    verified_encryption_key = transform_subject_number(
+        card_public_key, door_loop_size)
+
+    assert encryption_key == verified_encryption_key
+
+
+def find_loop_size(subject_number, public_key):
+    """
+    >>> find_loop_size(7, 5764801)
+    8
+    """
+    for loop_size, value in iterate_subject_number(subject_number):
+        if value == public_key:
+            return loop_size
+
+
+class FooSet:
+    foo_class = NotImplemented
+
+    @classmethod
+    def from_foos_text(cls, foos_text):
+        non_empty_lines = filter(None, foos_text.splitlines())
+
+        return cls(list(map(cls.foo_class.from_foo_text, non_empty_lines)))
+
+    def __init__(self, foos):
+        self.foos = foos
+
+
+challenge = Challenge()
+challenge.main()
