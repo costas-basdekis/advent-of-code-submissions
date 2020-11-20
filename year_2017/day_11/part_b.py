719e0a525b9bee6ee07c8db2afaf3e69667613b1
Costas Basdekis <costas.basdekis@gmail.com>
2020-11-20 11:14:03 +0000

    Submit 2017-11-B

---
+++ year_2017/day_11/part_b.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import utils
+from year_2017.day_11 import part_a
+
+
+class Challenge(utils.BaseChallenge):
+    def solve(self, _input, debug=False):
+        """
+        >>> Challenge().default_solve()
+        1524
+        """
+        directions = part_a.DirectionsRotator()\
+            .rotate_directions(_input.strip().split(','))
+        max_distance = 0
+        zero_point = utils.PointHex(0, 0)
+        point = zero_point
+        for direction in directions:
+            point = point.move(direction)
+            max_distance = max(max_distance, point.step_distance(zero_point))
+        return max_distance
+
+
+challenge = Challenge()
+challenge.main()
