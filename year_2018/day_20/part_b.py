b4ec72ca0c26ef899890f4196b7222ec1c4daaae
Costas Basdekis <costas.basdekis@gmail.com>
2020-11-20 11:14:03 +0000

    Submit 2018-20-B

---
+++ year_2018/day_20/part_b.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import utils
+from year_2018.day_20 import part_a
+
+
+class Challenge(utils.BaseChallenge):
+    def solve(self, _input):
+        """
+        >>> Challenge().default_solve()
+        8520
+        """
+        distances = part_a.Area.from_instruction_text(_input)\
+            .get_room_distances()
+        return sum(
+            1
+            for distance in distances.values()
+            if distance >= 1000
+        )
+
+
+challenge = Challenge()
+challenge.main()
