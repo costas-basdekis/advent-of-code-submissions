00817e725adc5b10ca4791c9c8ea56b1e5d634dc
Costas Basdekis <costas.basdekis@gmail.com>
2020-11-20 11:14:03 +0000

    Submit 2020-04-B

--- year_2020/day_04/part_a.py
+++ year_2020/day_04/part_a.py
@@ -18,63 +18,74 @@ def solve(_input=None):
     return len(PassportBatch.from_batch_text(_input).passports)
 
 
-class PassportBatch:
-    re_empty_lines = re.compile(r"\n\n+")
+class Field:
+    def parse_and_validate(self, value):
+        raise NotImplementedError()
 
-    @classmethod
-    def from_batch_text(cls, batch_text):
+
+class NumberField(Field):
+    NUMBERS = set("0123456789")
+
+    def __init__(self, min_value=None, max_value=None):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def parse_and_validate(self, value):
         """
-        >>> PassportBatch.from_batch_text("\\n" * 5).passports
-        []
-        >>> PassportBatch.from_batch_text(
-        ...     "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\\n"
-        ...     "byr:1937 iyr:2017 cid:147 hgt:183cm\\n"
-        ...     "\\n"
-        ...     "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\\n"
-        ...     "hcl:#cfa07d byr:1929\\n"
-        ...     "\\n"
-        ...     "hcl:#ae17e1 iyr:2013\\n"
-        ...     "eyr:2024\\n"
-        ...     "ecl:brn pid:760753108 byr:1931\\n"
-        ...     "hgt:179cm\\n"
-        ...     "\\n"
-        ...     "hcl:#cfa07d eyr:2025 pid:166559648\\n"
-        ...     "iyr:2011 ecl:brn hgt:59in        \\n"
-        ... ).passports
-        [Passport(birth_year=1937, issue_year=2017, expiration_year=2020, height=183, hair_colour='#fffffd', eye_colour='gry', id='860033327', country_id='147'), \
-Passport(birth_year=1931, issue_year=2013, expiration_year=2024, height=179, hair_colour='#ae17e1', eye_colour='brn', id='760753108', country_id=None)\
-]
+        >>> NumberField().parse_and_validate(1821)
+        (True, 1821)
+        >>> NumberField().parse_and_validate(-1821)
+        (True, -1821)
+        >>> NumberField(1700, 1900).parse_and_validate(1821)
+        (True, 1821)
+        >>> NumberField(1700, 1900).parse_and_validate(-1821)
+        (False, None)
+        >>> NumberField(1700, 1900).parse_and_validate(1621)
+        (False, None)
         """
-        passport_texts = filter(None, cls.re_empty_lines.split(batch_text))
-        passports = list(filter(
-            None, map(Passport.from_passport_text, passport_texts)))
+        try:
+            year = int(value)
+        except (ValueError, TypeError):
+            return False, None
 
-        return cls(passports)
+        if self.min_value is not None and year < self.min_value:
+            return False, None
 
-    def __init__(self, passports):
-        self.passports = passports
+        if self.max_value is not None and year > self.max_value:
+            return False, None
+
+        return True, year
 
 
-def parse_height(height_str):
-    """
-    >>> parse_height("187cm")
-    187
-    >>> parse_height("172in")
-    436.88
-    >>> parse_height("140")
-    140
-    >>> parse_height("1.6m")
-    Traceback (most recent call last):
-    ...
-    Exception: Not a valid height: 1.6m
-    """
-    if height_str.endswith("cm"):
-        return int(height_str[:-2])
-    if height_str.endswith("in"):
-        return int(height_str[:-2]) * 2.54
-    if set("1234567890").issuperset(set(height_str)):
-        return int(height_str)
-    raise Exception(f"Not a valid height: {height_str}")
+class HeightField(NumberField):
+    CmField = NumberField()
+    InField = NumberField()
+    PlainField = NumberField()
+
+    def parse_and_validate(self, value):
+        """
+        >>> HeightField().parse_and_validate("187cm")
+        (True, 187)
+        >>> HeightField().parse_and_validate("172in")
+        (True, 436.88)
+        >>> HeightField().parse_and_validate("140")
+        (True, 140)
+        >>> HeightField().parse_and_validate("1.6m")
+        (False, None)
+        """
+        if value.endswith("cm"):
+            return self.CmField.parse_and_validate(value[:-2])
+        if value.endswith("in"):
+            validated, parsed = self.InField.parse_and_validate(value[:-2])
+            if not validated:
+                return validated, parsed
+            return validated, parsed * 2.54
+        return self.PlainField.parse_and_validate(value)
+
+
+class NoValidationField(Field):
+    def parse_and_validate(self, value):
+        return True, value
 
 
 class Passport(namedtuple("Passport", (
@@ -103,14 +114,14 @@ class Passport(namedtuple("Passport", (
         "pid": "id",
     }
     FIELD_TRANSFORMATION_MAP = {
-        "byr": int,
-        "iyr": int,
-        "eyr": int,
-        "hgt": parse_height,
-        "hcl": str,
-        "ecl": str,
-        "cid": str,
-        "pid": str,
+        "byr": NumberField().parse_and_validate,
+        "iyr": NumberField().parse_and_validate,
+        "eyr": NumberField().parse_and_validate,
+        "hgt": HeightField().parse_and_validate,
+        "hcl": NoValidationField().parse_and_validate,
+        "ecl": NoValidationField().parse_and_validate,
+        "cid": NoValidationField().parse_and_validate,
+        "pid": NoValidationField().parse_and_validate,
     }
 
     @classmethod
@@ -158,15 +169,60 @@ class Passport(namedtuple("Passport", (
         )
         if has_required_fields_missing:
             return None
+        valid_parsed_fields = {
+            field: parsed
+            for field, (validated, parsed) in (
+                (cls.FIELD_NAME_MAP[field],
+                 cls.FIELD_TRANSFORMATION_MAP[field](value))
+                for field, value in raw_fields.items()
+            )
+            if validated
+        }
+        if len(valid_parsed_fields) != len(raw_fields):
+            return None
 
         return cls(**{
             "country_id": None,
-            **{
-                cls.FIELD_NAME_MAP[field]:
-                cls.FIELD_TRANSFORMATION_MAP[field](value)
-                for field, value in raw_fields.items()
-            },
+            **valid_parsed_fields,
         })
+
+
+class PassportBatch:
+    PASSPORT_CLASS = Passport
+    re_empty_lines = re.compile(r"\n\n+")
+
+    @classmethod
+    def from_batch_text(cls, batch_text):
+        """
+        >>> PassportBatch.from_batch_text("\\n" * 5).passports
+        []
+        >>> PassportBatch.from_batch_text(
+        ...     "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\\n"
+        ...     "byr:1937 iyr:2017 cid:147 hgt:183cm\\n"
+        ...     "\\n"
+        ...     "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\\n"
+        ...     "hcl:#cfa07d byr:1929\\n"
+        ...     "\\n"
+        ...     "hcl:#ae17e1 iyr:2013\\n"
+        ...     "eyr:2024\\n"
+        ...     "ecl:brn pid:760753108 byr:1931\\n"
+        ...     "hgt:179cm\\n"
+        ...     "\\n"
+        ...     "hcl:#cfa07d eyr:2025 pid:166559648\\n"
+        ...     "iyr:2011 ecl:brn hgt:59in        \\n"
+        ... ).passports
+        [Passport(birth_year=1937, issue_year=2017, expiration_year=2020, height=183, hair_colour='#fffffd', eye_colour='gry', id='860033327', country_id='147'), \
+Passport(birth_year=1931, issue_year=2013, expiration_year=2024, height=179, hair_colour='#ae17e1', eye_colour='brn', id='760753108', country_id=None)\
+]
+        """
+        passport_texts = filter(None, cls.re_empty_lines.split(batch_text))
+        passports = list(filter(
+            None, map(cls.PASSPORT_CLASS.from_passport_text, passport_texts)))
+
+        return cls(passports)
+
+    def __init__(self, passports):
+        self.passports = passports
 
 
 if __name__ == '__main__':
---
+++ year_2020/day_04/part_b.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+import doctest
+
+from utils import get_current_directory
+from year_2020.day_04 import part_a
+
+
+def solve(_input=None):
+    """
+    >>> solve()
+    186
+    """
+    if _input is None:
+        _input = get_current_directory(__file__)\
+            .joinpath("part_a_input.txt")\
+            .read_text()
+    return len(StrictPassportBatch.from_batch_text(_input).passports)
+
+
+class NoValueAcceptedField(part_a.Field):
+    def parse_and_validate(self, value):
+        return False, None
+
+
+class StrictHeightField(part_a.HeightField):
+    CmField = part_a.NumberField(150, 193)
+    InField = part_a.NumberField(59, 76)
+    PlainField = NoValueAcceptedField()
+
+    def parse_and_validate(self, value):
+        """
+        >>> StrictHeightField().parse_and_validate("187cm")
+        (True, 187)
+        >>> StrictHeightField().parse_and_validate("150cm")
+        (True, 150)
+        >>> StrictHeightField().parse_and_validate("193cm")
+        (True, 193)
+        >>> StrictHeightField().parse_and_validate("143cm")
+        (False, None)
+        >>> StrictHeightField().parse_and_validate("194cm")
+        (False, None)
+        >>> StrictHeightField().parse_and_validate("72in")
+        (True, 182.88)
+        >>> StrictHeightField().parse_and_validate("172in")
+        (False, None)
+        >>> StrictHeightField().parse_and_validate("140")
+        (False, None)
+        >>> StrictHeightField().parse_and_validate("1.6m")
+        (False, None)
+        """
+        return super().parse_and_validate(value)
+
+
+class ColourField(part_a.Field):
+    HEX_NUMBERS = set("0123456789abcdef")
+
+    def parse_and_validate(self, value):
+        if len(value) != len("#123456"):
+            return False, None
+
+        if value[0] != "#":
+            return False, None
+
+        if not set(value[1:]).issubset(self.HEX_NUMBERS):
+            return False, None
+
+        return True, value
+
+
+class FixedValuesField(part_a.Field):
+    def __init__(self, fixed_values):
+        self.fixed_values = set(fixed_values)
+
+    def parse_and_validate(self, value):
+        if value not in self.fixed_values:
+            return False, None
+
+        return True, value
+
+
+class PassportField(part_a.Field):
+    NUMBERS = set("0123456789")
+
+    def parse_and_validate(self, value):
+        if len(value) != 9:
+            return False, None
+
+        if not set(value).issubset(self.NUMBERS):
+            return False, None
+
+        return True, value
+
+
+class StrictPassport(part_a.Passport):
+    FIELD_TRANSFORMATION_MAP = {
+        **part_a.Passport.FIELD_TRANSFORMATION_MAP,
+        "byr": part_a.NumberField(1920, 2002).parse_and_validate,
+        "iyr": part_a.NumberField(2010, 2020).parse_and_validate,
+        "eyr": part_a.NumberField(2020, 2030).parse_and_validate,
+        "hgt": StrictHeightField().parse_and_validate,
+        "hcl": ColourField().parse_and_validate,
+        "ecl": FixedValuesField({
+            "amb", "blu", "brn", "gry", "grn", "hzl", "oth",
+        }).parse_and_validate,
+        "pid": PassportField().parse_and_validate,
+    }
+
+
+class StrictPassportBatch(part_a.PassportBatch):
+    PASSPORT_CLASS = StrictPassport
+
+    @classmethod
+    def from_batch_text(cls, batch_text):
+        """
+        >>> StrictPassportBatch.from_batch_text("\\n" * 5).passports
+        []
+        >>> StrictPassportBatch.from_batch_text(
+        ...     "eyr:1972 cid:100\\n"
+        ...     "hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926\\n"
+        ...     "\\n"
+        ...     "iyr:2019\\n"
+        ...     "hcl:#602927 eyr:1967 hgt:170cm\\n"
+        ...     "ecl:grn pid:012533040 byr:1946\\n"
+        ...     "\\n"
+        ...     "hcl:dab227 iyr:2012\\n"
+        ...     "ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277\\n"
+        ...     "\\n"
+        ...     "hgt:59cm ecl:zzz\\n"
+        ...     "eyr:2038 hcl:74454a iyr:2023\\n"
+        ...     "pid:3556412378 byr:2007\\n"
+        ... ).passports
+        []
+        >>> StrictPassportBatch.from_batch_text(
+        ...     "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\\n"
+        ...     "hcl:#623a2f\\n"
+        ...     "\\n"
+        ...     "eyr:2029 ecl:blu cid:129 byr:1989\\n"
+        ...     "iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm\\n"
+        ...     "\\n"
+        ...     "hcl:#888785\\n"
+        ...     "hgt:164cm byr:2001 iyr:2015 cid:88\\n"
+        ...     "pid:545766238 ecl:hzl\\n"
+        ...     "eyr:2022\\n"
+        ...     "\\n"
+        ...     "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719\\n"
+        ... ).passports
+        [StrictPassport(birth_year=1980, issue_year=2012, expiration_year=2030, height=187.96, hair_colour='#623a2f', eye_colour='grn', id='087499704', country_id=None), \
+StrictPassport(birth_year=1989, issue_year=2014, expiration_year=2029, height=165, hair_colour='#a97842', eye_colour='blu', id='896056539', country_id='129'), \
+StrictPassport(birth_year=2001, issue_year=2015, expiration_year=2022, height=164, hair_colour='#888785', eye_colour='hzl', id='545766238', country_id='88'), \
+StrictPassport(birth_year=1944, issue_year=2010, expiration_year=2021, height=158, hair_colour='#b6652a', eye_colour='blu', id='093154719', country_id=None)\
+]
+        >>> StrictPassportBatch.from_batch_text(
+        ...     "eyr:1972 cid:100\\n"
+        ...     "hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926\\n"
+        ...     "\\n"
+        ...     "iyr:2019\\n"
+        ...     "hcl:#602927 eyr:1967 hgt:170cm\\n"
+        ...     "ecl:grn pid:012533040 byr:1946\\n"
+        ...     "\\n"
+        ...     "hcl:dab227 iyr:2012\\n"
+        ...     "ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277\\n"
+        ...     "\\n"
+        ...     "hgt:59cm ecl:zzz\\n"
+        ...     "eyr:2038 hcl:74454a iyr:2023\\n"
+        ...     "pid:3556412378 byr:2007\\n"
+        ...     "\\n"
+        ...     "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\\n"
+        ...     "hcl:#623a2f\\n"
+        ...     "\\n"
+        ...     "eyr:2029 ecl:blu cid:129 byr:1989\\n"
+        ...     "iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm\\n"
+        ...     "\\n"
+        ...     "hcl:#888785\\n"
+        ...     "hgt:164cm byr:2001 iyr:2015 cid:88\\n"
+        ...     "pid:545766238 ecl:hzl\\n"
+        ...     "eyr:2022\\n"
+        ...     "\\n"
+        ...     "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719\\n"
+        ... ).passports
+        [StrictPassport(birth_year=1980, issue_year=2012, expiration_year=2030, height=187.96, hair_colour='#623a2f', eye_colour='grn', id='087499704', country_id=None), \
+StrictPassport(birth_year=1989, issue_year=2014, expiration_year=2029, height=165, hair_colour='#a97842', eye_colour='blu', id='896056539', country_id='129'), \
+StrictPassport(birth_year=2001, issue_year=2015, expiration_year=2022, height=164, hair_colour='#888785', eye_colour='hzl', id='545766238', country_id='88'), \
+StrictPassport(birth_year=1944, issue_year=2010, expiration_year=2021, height=158, hair_colour='#b6652a', eye_colour='blu', id='093154719', country_id=None)\
+]
+        """
+        return super().from_batch_text(batch_text)
+
+
+if __name__ == '__main__':
+    if doctest.testmod(part_a).failed or doctest.testmod().failed:
+        print("Tests failed")
+    else:
+        print("Tests passed")
+    print("Solution:", solve())
