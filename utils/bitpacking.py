f5126f900ef440f0eaf349fa8bbf106c559e50f6
Costas Basdekis <costas.basdekis@gmail.com>
2020-11-20 11:14:03 +0000

    Submit 2016-13-A

--- utils/bitpacking.py
+++ utils/bitpacking.py
@@ -1,31 +1,37 @@
 import math
 from functools import reduce
-from typing import List, Iterable
+from typing import Iterable, Callable, Optional
 
-__all__ = ['int_to_bits', 'bits_to_int']
+__all__ = ['int_to_bits', 'bits_to_int', 'get_bit_count']
 
 
-def int_to_bits(_int: int) -> List[int]:
+Ints = Iterable[int]
+
+
+def int_to_bits(number: int, container: Optional[Callable[[Ints], Ints]] = None,
+                ) -> Ints:
     """
-    >>> list(map(int_to_bits, range(8)))
+    >>> # noinspection PyUnresolvedReferences
+    >>> [int_to_bits(x, list) for x in  range(8)]
     [[], [1], [2], [1, 2], [4], [1, 4], [2, 4], [1, 2, 4]]
-    >>> int_to_bits(2 ** 16)
+    >>> int_to_bits(2 ** 16, list)
     [65536]
-    >>> int_to_bits(2 ** 16 - 1)
+    >>> int_to_bits(2 ** 16 - 1, list)
     [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384,
         32768]
     """
-    if not _int:
+    if not number:
         return []
-    bits = int(math.log(_int, 2) + 1)
-    return [
-        2 ** index
-        for index in range(bits)
-        if 2 ** index & _int
-    ]
+    bit_count = int(math.log(number, 2))
+    powers = map((2).__pow__, range(bit_count + 1))
+    bits = filter(number.__and__, powers)
+    if container is not None:
+        bits = container(bits)
+
+    return bits
 
 
-def bits_to_int(bits: Iterable[int]) -> int:
+def bits_to_int(bits: Ints) -> int:
     """
     >>> list(map(bits_to_int,
     ...     [[], [1], [2], [1, 2], [4], [1, 4], [2, 4], [1, 2, 4]]))
@@ -37,3 +43,17 @@ def bits_to_int(bits: Iterable[int]) -> int:
     65535
     """
     return reduce(int.__or__, bits, 0)
+
+
+def get_bit_count(number: int) -> int:
+    """
+    >>> # noinspection PyUnresolvedReferences
+    >>> [get_bit_count(x) for x in  range(8)]
+    [0, 1, 1, 2, 1, 2, 2, 3]
+    >>> get_bit_count(2 ** 16)
+    1
+    >>> get_bit_count(2 ** 16 - 1)
+    16
+    """
+    from utils import helper
+    return helper.iterable_length(int_to_bits(number))
---
+++ year_2016/day_13/part_a.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+from dataclasses import dataclass, field
+from itertools import product
+from typing import Dict, Iterable, List, Optional, Type
+
+from aox.utils import Timer
+
+from utils import BaseChallenge, min_and_max_tuples, get_bit_count, Self, \
+    Point2D
+
+
+class Challenge(BaseChallenge):
+    def solve(self, _input, debug=False):
+        """
+        >>> Challenge().default_solve()
+        90
+        """
+        maze = Maze(int(_input))
+        solution = maze.solve(Point2D(1, 1), Point2D(31, 39))
+        if debug:
+            print(maze.show(solution=solution))
+
+        return len(solution) - 1
+
+
+Position = Point2D
+
+
+class MazeSolver:
+    def get_solution_length(self, maze: 'Maze', start: Position, end: Position,
+                            ) -> int:
+        """
+        >>> MazeSolver().get_solution_length(
+        ...     Maze(10).fill(product(range(10), range(7))),
+        ...     Point2D(1, 1), Point2D(1, 1))
+        0
+        >>> MazeSolver().get_solution_length(
+        ...     Maze(10).fill(product(range(10), range(7))),
+        ...     Point2D(1, 1), Point2D(7, 4))
+        11
+        >>> MazeSolver().get_solution_length(
+        ...     Maze(10), Point2D(1, 1), Point2D(7, 4))
+        11
+        """
+        return len(self.solve(maze, start, end)) - 1
+
+    def solve(self, maze: 'Maze', start: Position, end: Position,
+              debug: bool = False) -> List[Position]:
+        """
+        >>> _maze = Maze(10).fill(product(range(10), range(7)))
+        >>> solution = MazeSolver().solve(_maze, Point2D(1, 1), Point2D(7, 4))
+        >>> print(_maze.show(solution=solution))
+        .#.####.##
+        .O#..#...#
+        #OOO.##...
+        ###O#.###.
+        .##OO#.O#.
+        ..##OOOO#.
+        #...##.###
+        >>> bare_maze = Maze(10)
+        >>> bare_solution = MazeSolver().solve(
+        ...     bare_maze, Point2D(1, 1), Point2D(7, 4))
+        >>> bare_solution
+        [Point2D(x=1, y=1), Point2D(x=1, y=2), Point2D(x=2, y=2),
+            Point2D(x=3, y=2), Point2D(x=3, y=3), Point2D(x=3, y=4),
+            Point2D(x=4, y=4), Point2D(x=4, y=5), Point2D(x=5, y=5),
+            Point2D(x=6, y=5), Point2D(x=7, y=5), Point2D(x=7, y=4)]
+        """
+        if start == end:
+            return [start]
+        stack = [start]
+        previous = {start: None}
+        if debug:
+            step = 0
+            timer = Timer()
+        while stack:
+            position = stack.pop(0)
+            neighbours = position.get_manhattan_neighbours()
+            for neighbour in neighbours:
+                if neighbour in previous:
+                    continue
+                if maze.get(neighbour):
+                    continue
+                previous[neighbour] = position
+                if neighbour == end:
+                    return self.get_solution(end, previous)
+                stack.append(neighbour)
+
+            if debug:
+                step += 1
+                if step % 1000 == 0:
+                    print(
+                        f"Step: {step}, time: "
+                        f"{timer.get_pretty_current_duration(0)}, seen: "
+                        f"{len(previous)}, stack: {len(stack)}")
+
+    def get_solution(self, end: Position,
+                     previous: Dict[Position, Optional[Position]],
+                     ) -> List[Position]:
+        solution = []
+        position = end
+        while position:
+            solution.insert(0, position)
+            position = previous[position]
+
+        return solution
+
+
+@dataclass
+class Maze:
+    designers_favourite_number: int
+    contents: Dict[Position, bool] = field(default_factory=dict)
+
+    def get_solution_length(self, start: Position, end: Position,
+                            solver: Type['MazeSolver'] = MazeSolver) -> int:
+        return solver().get_solution_length(self, start, end)
+
+    def solve(self, start: Position, end: Position,
+              solver: Type['MazeSolver'] = MazeSolver) -> List[Position]:
+        return solver().solve(self, start, end)
+
+    def fill(self: Self['Maze'], positions: Iterable[Position]) -> Self['Maze']:
+        for position in positions:
+            self.get(position)
+        return self
+
+    def get(self, position: Position) -> bool:
+        if position not in self.contents:
+            self.contents[position] = self.is_position_wall(position)
+        return self.contents[position]
+
+    def is_position_wall(self, position: Position) -> bool:
+        base_value = self.get_base_value(position)
+        total_value = base_value + self.designers_favourite_number
+        return get_bit_count(total_value) % 2 == 1
+
+    def get_base_value(self, position: Position) -> int:
+        x, y = position
+        x_y = x + y
+        return x_y * (x_y + 3) - 2 * y
+
+    SHOW_MAP = {
+        False: ".",
+        True: "#",
+        None: " ",
+    }
+    SHOW_SOLUTION = "O"
+
+    def show(self, solution: Optional[Iterable[Position]] = None) -> str:
+        """
+        >>> print(Maze(10).show())
+        <BLANKLINE>
+        >>> print(Maze(10).fill(product(range(10), range(7))).show())
+        .#.####.##
+        ..#..#...#
+        #....##...
+        ###.#.###.
+        .##..#..#.
+        ..##....#.
+        #...##.###
+        """
+        _, (max_x, max_y) = min_and_max_tuples(self.contents, Point2D(0, 0))
+        if solution:
+            solution = set(solution)
+        if not solution:
+            solution = set()
+        return "\n".join(
+            "".join(
+                self.SHOW_SOLUTION
+                if Point2D(x, y) in solution else
+                self.SHOW_MAP[self.contents.get(Point2D(x, y))]
+                for x in range(max_x + 1)
+            ).rstrip()
+            for y in range(max_y + 1)
+        )
+
+
+Challenge.main()
+challenge = Challenge()
