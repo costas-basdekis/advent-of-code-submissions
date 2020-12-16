d57f1980589e86e598f6fde8a56fc0aad55235e3
Costas Basdekis <costas.basdekis@gmail.com>
2020-12-16 13:49:14 +0000

    Update example part

--- example_year/example_day/example_part.py
+++ example_year/example_day/example_part.py
@@ -1,23 +1,14 @@
 #!/usr/bin/env python3
-import doctest
-
-from utils import get_current_directory
+import utils
 
 
-def solve(_input=None):
-    """
-    >>> solve()
-    42
-    """
-    if _input is None:
-        _input = get_current_directory(__file__)\
-            .joinpath("part_a_input.txt")\
-            .read_text()
+class Challenge(utils.BaseChallenge):
+    def solve(self, _input):
+        """
+        >>> Challenge().default_solve()
+        42
+        """
 
 
-if __name__ == '__main__':
-    if doctest.testmod().failed:
-        print("Tests failed")
-    else:
-        print("Tests passed")
-    print("Solution:", solve())
+challenge = Challenge()
+challenge.main()
