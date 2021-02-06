c737b59f9f4bf5f84c9ab946a64f0c8916b0495c
Costas Basdekis <costas.basdekis@gmail.com>
2021-02-06 13:07:02 +0000

    Simpler way to get utils test modules

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,34 +4,28 @@ from .base_challenge import *
 from .helper import *
 from .point import *
 
-# noinspection PyProtectedMember
-from .base_challenge import __all__ as base_challenge_all
-# noinspection PyProtectedMember
-from .helper import __all__ as helper_all
-# noinspection PyProtectedMember
-from .point import __all__ as point_all
-
-__all__ = [
-    'test_modules_names', 'test_modules',
-] + (
-    base_challenge_all
-    + helper_all
-    + point_all
-)
-
-test_modules_names = [
-    'base_challenge',
-    'helper',
-    'point',
+test_modules = [
+    importlib.import_module('utils.base_challenge'),
+    importlib.import_module('utils.helper'),
+    importlib.import_module('utils.point'),
 ]
+
+# noinspection PyUnresolvedReferences
+__all__ = [
+    'test_modules',
+] + sum((
+    test_module.__all__
+    for test_module in test_modules
+), [])
+
 
 if __name__ == "__main__":
     import doctest
     optionflags = doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE
     succeeded, failed = 0, 0
-    for name in test_modules_names:
+    for test_module in test_modules:
         results = doctest.testmod(
-            importlib.import_module(name),
+            test_module,
             optionflags=optionflags,
         )
         succeeded += results.attempted - results.failed
@@ -40,8 +34,3 @@ if __name__ == "__main__":
         print(f"{failed} tests failed")
     else:
         print(f"{succeeded} tests passed")
-else:
-    test_modules = [
-        importlib.import_module(f'utils.{name}')
-        for name in test_modules_names
-    ]
