af02d4c19ab1793e1974f5f76b30273b56214c5f
Costas Basdekis <costas.basdekis@gmail.com>
2023-12-12 13:42:03 +0000

    Submit 2022-25-B

---
+++ year_2022/day_25/part_b.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from dataclasses import dataclass, field
+import re
+from enum import Enum
+from typing import (
+    Any, cast, Callable, ClassVar, Dict, Generic, Iterable, List, Optional, Set,
+    Tuple, Type, Union, TypeVar,
+)
+
+from aox.challenge import Debugger
+from utils import (
+    BaseChallenge, Point2D, get_type_argument_class, helper, Cls, Self,
+)
+
+
+class Challenge(BaseChallenge):
+    def solve(self, _input: str, debugger: Debugger) -> Union[str, int]:
+        """
+        >>> Challenge().default_solve()
+        42
+        """
+
+
+Challenge.main()
+challenge = Challenge()
