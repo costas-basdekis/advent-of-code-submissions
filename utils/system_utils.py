97284705abf81ca88a0bcde703f96fea084fc444
Costas Basdekis <costas.basdekis@gmail.com>
2025-10-24 13:52:16 +0300

    WIP

--- utils/system_utils.py
+++ utils/system_utils.py
@@ -1,11 +1,14 @@
 import os
 import sys
 
-from typing import NoReturn
+from typing import Callable, List, NoReturn, Optional
 
 
 __all__ = ["restart_process"]
 
 
-def restart_process() -> NoReturn:
-    return os.execl(sys.executable, sys.executable, *sys.argv)
+def restart_process(edit_args: Optional[Callable[[List[str]], List[str]]] = None) -> NoReturn:
+    args = sys.argv
+    if edit_args:
+        args = edit_args(args)
+    return os.execl(sys.executable, sys.executable, *args)
